97eec50bc353adbd4cee4f88aed8b28b32dfd7bc
Lucius-GG <gwintmail@gmail.com>
2026-05-28 23:42:32 +0200

    Ordner-Struktur-Fix

--- src/Manager.py
+++ src/Manager.py
@@ -1,7 +1,11 @@
 import json
 import os
+import sys
 from datetime import datetime
 from typing import Optional, Dict, List
+
+sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
+
 from src.ToDoListeKlassen import Task
 
 _STORAGE_FILENAME = "tasks.json"
